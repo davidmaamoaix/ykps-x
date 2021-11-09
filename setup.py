d3dbd8e8e6d37624a5d979f5769cba0262c38cff
BuildTools <davidmaamoaix@gmail.com>
2021-11-09 08:10:58 +0800

    oops

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (PATH / 'README.md').read_text()
 
 setup(
     name='ykps-x',
-    version='0.1.0',
+    version='0.1.2',
     description='The stupid YKPS portal, now in your CLI.',
     long_description=README,
     long_description_content_type='text/markdown',
